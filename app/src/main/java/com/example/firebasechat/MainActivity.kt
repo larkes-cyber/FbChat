b11102c1ac49f66c49e6058ba3fd9f711b951faa
larkes-cyber <playegrovoy@gmail.com>
2022-06-29 11:33:43 +0300

    work with on change listener of messages

--- app/src/main/java/com/example/firebasechat/MainActivity.kt
+++ app/src/main/java/com/example/firebasechat/MainActivity.kt
@@ -2,20 +2,49 @@ package com.example.firebasechat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.FirebaseDatabase
+import com.example.firebasechat.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.button.setOnClickListener {
+            myRef.setValue(binding.inpMessage.text.toString())
+        }
+        onChangeListener(myRef)
+    }
+
+    private fun onChangeListener(dRef:DatabaseReference){
+
+        dRef.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Sergey: ${snapshot.value.toString()}")
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
     }
+
 }
